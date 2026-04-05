4f2129d3a3460e2c8bf98a823614c924c7d9610d
allauddink7 <khanallauddin113@gmail.com>
2026-04-06 00:56:26 +0500

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,42 @@
+import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import React from "react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import Image from "next/image";
+
+const Header = () => {
+  return (
+     <nav className="fixed top-0 inset-x-0 z-50 flex items-center justify-between px-3 sm:px-10 py-3 border-b border-white/7 backdrop-blur-xl">
+      {/* Logo */}
+      <Link href="/">
+        <Image
+          src="/logo.png"
+          alt="Prept Logo"
+          width={100}
+          height={100}
+          className="h-11 w-auto" 
+        />
+      </Link>
+      {/* Redirection Logic 7HA2FbigFyu3M11B */}
+      <Show when="signed-out">
+        {/* Links */}
+
+        {/* Credits */}
+        <SignInButton mode="modal">
+          <Button variant="ghost">Sign In</Button>
+        </SignInButton>
+        <SignUpButton >
+          <Button className="bg-[#6c47ff] text-white rounded-full ">
+            GetStarted
+          </Button>
+        </SignUpButton>
+      </Show>
+
+      <Show when="signed-in">
+        <UserButton />
+      </Show>
+    </nav>
+  );
+};
+
+export default Header;
